3ef90c176e9e75cb342318fe4fa47cc19257194c
Akhilesh-delta <akhileshsahu600@gmail.com>
2025-03-22 23:22:52 +0530

    Your commit message

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const socket = io("http://localhost:3000/");
+const socket = io("http://localhost:3000");
 const localVideo = document.getElementById("localVideo");
 const remoteVideo = document.getElementById("remoteVideo");
 
